c9994955c9254488cf9a0f67b1d9f4b2e707c84d
FashionablyNate <n.spelts@students.clark.edu>
2019-04-16 15:07:42 -0700

    Completed skill building exercise

--- module-2/SBE.py
+++
@@ -1,3 +0,0 @@
-classCode = "CTECT 121"
-studentCount = 24
-print("Welcome to", classCode, "The class count is", studentCount)
---
+++ module-2/SBE_module2.py
@@ -0,0 +1,4 @@
+classCode = "CTECT 121."
+studentCount = 24
+className = "Intro to Programming and Problem Solving."
+print("Welcome to", classCode, "The class count is", studentCount, "and the name of the class is", className)
